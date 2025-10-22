ec0cd843893dcb074b3ed155a7d9d9846e851c77
Ubuntu <ubuntu@ip-172-31-20-59.ap-south-1.compute.internal>
2025-10-22 09:13:26 +0000

    DatabaseSeeder file updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('test123'),
+            'address' => '123 Main St',
+            'phone_no' => '2222222222',
+            'isAdmin' => false,
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
